7941dd68fd1ea1c285383d8c39b94ca7d20ca509
juliano bresolin <bresolinfotografia@gmail.com>
2023-11-21 12:41:31 -0300

    fav

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ export const Loader = () => {
 	return (
 		<div className="h-full flex flex-col gap-y-4 items-center justify-center">
 			<div className=" relative animate-bounce">
-				<Image alt="Loader" src="/Loader.png" width={60} height={86} />
+				<Image alt="Loader" src="/loader.png" width={60} height={86} />
 			</div>
 			<p className="text-sm text-muted-foreground">Processando...</p>
 		</div>
--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const BotAvatar = () => {
 	return (
-		<Avatar className="h-[43px] w-[30px]">
+		<Avatar className="h-[43px] w-[32px]">
 			<AvatarImage className="p-1" src="/avatarprokit.png" />
 		</Avatar>
 	);
