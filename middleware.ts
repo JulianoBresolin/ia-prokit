058a18ebe16ed50199966c1812d451d881283148
juliano bresolin <bresolinfotografia@gmail.com>
2023-12-12 11:06:56 -0300

    edgestore

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/api/webhook", "/public"],
+	publicRoutes: ["/", "/api/webhook", "/public", "/api/edgestore/init"],
 });
 
 export const config = {
