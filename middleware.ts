2008728ea19bf9b3202af7e6dfff9e64359604ca
juliano bresolin <bresolinfotografia@gmail.com>
2023-12-15 16:39:57 -0300

    crontest

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/api/webhook", "/public", "/api/edgestore/init"],
+	publicRoutes: [
+		"/",
+		"/api/webhook",
+		"/public",
+		"/api/edgestore/init",
+		"/politica-de-privacidade",
+		"api/cron",
+	],
 });
 
 export const config = {
