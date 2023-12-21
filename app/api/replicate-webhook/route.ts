4a0114d21da5b4c3d0d5986558ce519cd9199ccf
juliano bresolin <bresolinfotografia@gmail.com>
2023-12-21 15:42:25 -0300

    del

--- app/api/replicate-webhook/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export default async function GET(req: Request) {
-	console.log(" incoming webhook!", req.body);
-	const prediction = req.body;
-
-	return NextResponse.json(prediction);
-}
