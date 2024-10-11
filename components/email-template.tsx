dc45fb441b292adfc00e6b182922db9df68514f5
juliano bresolin <bresolinfotografia@gmail.com>
2024-10-10 22:21:16 -0300

    border

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -14,7 +14,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
 		<h1>
 			Welcome, {firstName} !
 			<a href={imageUrl}>
-				<img border="0" alt="img" src={imageUrl} width="100" height="100" />
+				<img alt="img" src={imageUrl} width="100" height="100" />
 			</a>
 		</h1>
 	</div>
